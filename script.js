c4f5b0432a0e6885c06c3efe4ae4377cb333f3b6
ErikG <erik.grootoonk@gmail.com>
2024-03-27 22:31:12 +0100

    console, types

---
+++ script.js
@@ -0,0 +1,67 @@
+const arr = [1, 2, 3, 4];
+
+arr.push(5);
+
+console.log(arr);
+
+const person = {
+  name: 'Brad',
+};
+
+person.email = ('brad@shitmail.com');
+
+console.log(person);
+
+// Declare multiple values at once
+
+let a, b, c;
+
+const d = 10, e = 20, f = 30;
+
+console.log(d + f);
+
+// Data Types -------------
+
+// String
+
+const firstname = 'Sara';
+
+console.log(firstname, typeof firstname);
+
+// Number
+const age = 30;
+const temp = 98.9;
+
+// Null
+const aptNumber = null;
+
+// null is not an object, but because can also be represented as 0, it was decided with
+// the first implementation to call it an object
+
+
+// Undefined
+let score;
+
+
+// Symbol
+
+const id = Symbol('id');
+
+
+// BingInt
+
+const n = 9078475774209874597598n;
+
+
+
+const output = n
+
+console.log(output, typeof output);
+
+
+
+
+
+
+
+
